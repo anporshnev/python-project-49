03a33d02bc0831eb1e00f4b27095b8a4d71ba2e8
AndrewPorshnev <andrew.porshnev@yandex.ru>
2024-11-05 14:12:13 +0300

    Добавляет приветствие

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+import brain_games.cli
+from brain_games.cli import welcome_user
 
 
 def main():
-    print("Welcome to the Brain Games!")
+    welcome_user()
 
 
 if __name__ == '__main__':
